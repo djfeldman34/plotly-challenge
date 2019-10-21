9102f940106e5b2b0ed622cf3ae02d448e950410
DJ Feldman <djfeldman92@gmail.com>
2019-10-21 14:36:24 -0400

    a

--- Belly-Button-Diversity/static/js/app.js
+++ Belly-Button-Diversity/static/js/app.js
@@ -6,39 +6,70 @@ function buildMetadata(sample) {
   // document.getElementById("#sample-metadata").innerHTML = "";
   d3.json(url).then(function(response) {
     console.log(response);
-    $("#sample-metadata").empty();
+    metadata.html("");
     Object.entries(response).forEach(([key, value]) => metadata.append("p").text(`${key}: ${value}`));
   });
 }
-    // Use d3 to select the panel with id of `#sample-metadata`
-  
-    
-    
-
-    // Use `.html("") to clear any existing metadata
-  
-
-    // Use `Object.entries` to add each key and value pair to the panel
-  
-  
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-}
+   
 
 function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var url2 = "/samples/" + sample;
 
-    // @TODO: Build a Bubble Chart using the sample data
+  d3.json(url2).then(function(response) {
+    console.log(response);
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+    // var total_length = length(response.sample_values);
+
+    var sample_values2 = [];
+    var otu_ids2 = [];
+    var otu_labels2 = []; 
+
+    for (var i = 1; i < 10; i++){
+     sample_values2.push(response.sample_values[i]);
+     otu_ids2.push(response.otu_ids[i]);
+     otu_labels2.push(response.otu_labels[i]);
+    }
+  
+ 
+    var trace1 = {
+      values: sample_values2,
+      labels: otu_ids2.map(String),
+      hovertext: otu_labels2,
+      type: 'pie'
+    };
+
+    var trace2 = {
+      x: response.otu_ids,
+      y: response.sample_values,
+      mode: 'markers',
+      marker: {
+        size: response.sample_values
+      },
+    };
+
+    var data1 = [trace1];
+    var data2 = [trace2];
+
+    var layout1 = {
+      height: 400,
+      width: 400,
+    };
+
+    var layout2 = {
+      width: 1200,
+      height: 500
+    };
+    
+    var piepie = d3.select('#pie');
+
+    var bubbly = d3.select('#bubble');
+
+    Plotly.newPlot('pie', data1, layout1);
+    Plotly.newPlot('bubble', data2, layout2);
+
+  });
 }
-
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
